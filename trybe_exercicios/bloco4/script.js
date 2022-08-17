fb3e2cef1f51bd61da59deafd444a2fe1a883b67
Eduardo.Sousa <dudus.sousa4002@gmail.com>
2022-08-17 14:05:42 -0300

    concluido exercicio 5

--- trybe_exercicios/bloco4/script.js
+++ trybe_exercicios/bloco4/script.js
@@ -21,14 +21,43 @@
 
 //---Exercise 3
 
-const resultNota = 80;
+// const resultNota = 80;
 
-if (resultNota >= 80){
-    console.log("Parabéns, você foi aprovado(a)")
-}
-else if(resultNota < 80 && resultNota >= 60){
-    console.log("Você está na lista de espera")
-}
-else{
-    console.log("Você foi reprovado(a)")
-}
+// if (resultNota >= 80){
+//     console.log("Parabéns, você foi aprovado(a)")
+// }
+// else if(resultNota < 80 && resultNota >= 60){
+//     console.log("Você está na lista de espera")
+// }
+// else{
+//     console.log("Você foi reprovado(a)")
+// }
+
+//---Exercise 4
+
+// const currentHour = 4;
+// let message;
+
+// if (currentHour >= 22 ){
+//     message = "Não deveríamos comer nada, é hora de dormir"
+// }
+// else if(currentHour >= 18 && currentHour < 22){
+//     message = "Rango da noite, vamos jantar :D"
+// }
+// else if(currentHour >= 14 && currentHour < 18){
+//     message="Vamos fazer um bolo para o café da tarde?"
+// }
+// else if (currentHour >=11 && currentHour <14){
+//     message ="Hora do almoço!!!"
+// }
+// else if (currentHour >=4 && currentHour <11) {
+//     message= "Hmmm, cheiro de café recém passado"
+// }
+// else{
+//     message ="Dormindo"
+// }
+
+// console.log(message)
+
+
+//---Exercice 5
