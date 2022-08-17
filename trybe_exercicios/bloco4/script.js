ed553f070f47eda1b98b41e499188c848186e502
Eduardo.Sousa <dudus.sousa4002@gmail.com>
2022-08-17 13:29:58 -0300

    primeiro exercicio concluido

--- trybe_exercicios/bloco4/script.js
+++ trybe_exercicios/bloco4/script.js
@@ -1,7 +1,11 @@
 const myName = "Eduardo";
 const birthCity ="Piraju";
-let brithYear = 2000;
+let birthYear = 2000;
+
+birthYear = 2030;
 
 console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
 
 
