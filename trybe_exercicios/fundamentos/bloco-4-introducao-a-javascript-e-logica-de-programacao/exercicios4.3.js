2ed11eeeec81481909d3b8db3cf0ce510a5b51a3
Eduardo.Sousa <dudus.sousa4002@gmail.com>
2022-08-22 16:02:53 -0300

    realizado os exercicios que tem no conteudo

---
+++ trybe_exercicios/fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/exercicios4.3.js
@@ -0,0 +1,19 @@
+// let fruits = [3, 4, 10, 1, 12];
+// var total = 0;
+
+// for(let i = 0; i < fruits.length; i+=1){
+//     total +=fruits[i];
+// }
+
+// if (total > 15){
+//     console.log (total)
+// }else {
+//   console.log('Variavel menor que 16')
+// }
+
+let result = 0 ;
+
+for (let i = 1; i < 101; i+=1){
+  result+=i
+}
+console.log(result);
