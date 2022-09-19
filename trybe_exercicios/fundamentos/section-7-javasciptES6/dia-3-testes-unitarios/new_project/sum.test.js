6d390fed9b4896c4f5ccb4cffbdb23d8a7ee4441
Eduardo.Sousa <dudus.sousa4002@gmail.com>
2022-09-19 16:29:13 -0300

    exercicios do dia 3 seção 7

---
+++ trybe_exercicios/fundamentos/section-7-javasciptES6/dia-3-testes-unitarios/new_project/sum.test.js
@@ -0,0 +1,12 @@
+// const sum = (a, b) => a + b;
+
+// test('soma dois valores', () => {
+//   expect(sum(2, 3)).toEqual(5);
+// });
+
+// sum.test.js
+const sum = require('./sum');
+
+test('soma dois valores', () => {
+  expect(sum(2, 3)).toBe(5);
+});
