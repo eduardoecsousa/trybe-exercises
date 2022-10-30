c96ced79ef834ba56084942701c0e2f7121cade0
Eduardo.Sousa <dudus.sousa4002@gmail.com>
2022-10-30 19:44:37 -0300

    concluido aula

--- trybe_exercicios/2- Front-End/seção4/ciclo-de-vida/src/App.js
+++ trybe_exercicios/2- Front-End/seção4/ciclo-de-vida/src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Joke from './Components/Joke';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class DadJoke extends React.Component {
+  constructor() {
+    super();
+
+    this.saveJoke = this.saveJoke.bind(this);
+
+    this.state = {
+      jokeObj: undefined,
+      loading: true,
+      storedJokes: [],
+    }
+  }
+
+  async fetchJoke() {
+    this.setState(
+      { loading: true },
+      async () => {
+        const requestHeaders = { headers: { Accept: 'application/json' } }
+        const requestReturn = await fetch('https://icanhazdadjoke.com/', requestHeaders)
+        const requestObject = await requestReturn.json();
+        this.setState({
+          loading: false,
+          jokeObj: requestObject,
+        });
+      });
+  }
+
+  componentDidMount() {
+    this.fetchJoke();
+  }
+  
+  saveJoke() {
+    this.setState(({ storedJokes, jokeObj}) => ({
+      storedJokes:[...storedJokes, jokeObj]
+    }))
+
+    this.fetchJoke();
+  }
+
+  render() {
+    const { storedJokes, loading, jokeObj } = this.state;
+    const loadingElement = <span>Loading...</span>;
+
+    return (
+      <div>
+        <span>
+          {storedJokes.map(({ id, joke }) => (<p key={id}>{joke}</p>))}
+        </span>
+
+      <p>
+        {
+         loading 
+          ? loadingElement 
+          : <Joke jokeObj={ jokeObj } saveJoke={ this.saveJoke } />
+        }
+      </p>
+
+      </div>
+    );
+  }
 }
 
-export default App;
+export default DadJoke;
